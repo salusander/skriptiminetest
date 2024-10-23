0942cd114e18ea28b9cd85d1c4a1643b71c4761d
Sander Salu <sander.salu.002@voco.ee>
2024-10-23 12:48:28 +0300

    See on esimene skript

---
+++ praks1/skripttest.sh
@@ -0,0 +1,12 @@
+!#/bin/bash
+### See on testimiseks esimene skript
+
+echo "Oled kasutaja nimega: "
+whoami
+sleep 1
+echo "Ja sina asud kataloogis: "
+pwd
+sleep 1
+echo "Selle kataloogi sisu(list) on : "
+ls -l
+
